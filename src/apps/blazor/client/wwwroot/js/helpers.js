876a03127a7999a43272cebef0525ba1f93e3412
ahmedgalal007 <ahmedgalal007@hotmail.com>
2026-02-17 09:23:03 +0200

    Fix Desktop

---
+++ src/apps/blazor/client/wwwroot/js/helpers.js
@@ -0,0 +1,15 @@
+﻿if (!window.helpers) window.helpers = {}
+window.helpers.getWindowDimensions = function () {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+}
+window.helpers.registerResizeCallback = (dotnetHelper) => {
+    window.addEventListener("resize", () => {
+        dotnetHelper.invokeMethodAsync("OnBrowserResize", window.innerWidth, window.innerHeight);
+    });
+};
+
+window.helpers.launchApp = () => { }
+
