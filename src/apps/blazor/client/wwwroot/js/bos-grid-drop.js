196bf44d939ee1fca43440d96c450b5085154d39
ahmedgalal007 <ahmedgalal007@hotmail.com>
2026-02-20 07:07:09 +0200

    Fix DragDrop

--- src/apps/blazor/client/wwwroot/js/bos-grid-drop.js
+++ src/apps/blazor/client/wwwroot/js/bos-grid-drop.js
@@ -7,38 +7,56 @@ window.bos.gridDrop = {
     _hoveredIndex: null,
     _isDragging: false,
     _isDragOverFirst: true,
-    init: function(gridId, dotNetHelper) { },
-    ondrag: function(e, target) {
+    // init: function(gridId, dotNetHelper) {
+    init: function(target, dragable) {
+        console.log("Initializing Grid Drop Target", target);
+        console.log("Initializing Grid Drop dragable", dragable);
+        document.querySelectorAll(dragable).forEach(el => {
+            el.addEventListener("drag", this.ondrag)
+            el.addEventListener("dragstart", this.ondragstart)
+        });
+        document.querySelectorAll(target).forEach(el => {
+            el.addEventListener("dragover", this.ondragover)
+            el.addEventListener("dragenter", this.ondragenter)
+            el.addEventListener("dragleave", this.ondragleave)
+            el.addEventListener("drop", this.ondrop)
+            el.addEventListener("dragend", this.ondragend)
+        });
+    },
+    ondrag: function(e) {
         // console.log(target, "drag fired");
         this._dragItem = target;
     },
-    ondragstart: function(e, target) {
+    ondragstart: function(e) {
         // console.log(event.target, "dragstart fired");
         this._dragItem = target;
     },
-    ondragover: function(e, target) {
-        // e.preventDefault();
+    ondragover: function(e) {
+
+        e.preventDefault();
+        console.log("dragover fired")
         if (this._isDragOverFirst) {
             // First time dragging over the grid
             this._isDragOverFirst = false;
         }
     },
-    ondragenter: function(e, target) {
+    ondragenter: function(e) {
         console.log(target, "Enter Fierd");
-        target.classList.add("highlighted")
+        e.target.classList.add("highlighted")
     },
-    ondragleave: function(e, target) {
+    ondragleave: function(e) {
         
         console.log("Leave Fierd");
-        target.classList.remove("highlighted")
+        e.target.classList.remove("highlighted")
     },
-    ondrop: function(e, target) {
+    ondrop: function(e) {
         // e.preventDefault();
         console.log("Drop Fierd");
-        target.classList.remove("highlighted")
-        target.append(this._dragItem);
+        e.target.classList.remove("highlighted")
+        e.target.append(this._dragItem);
     },
-    ondragend: function(e, target) {
+    ondragend: function(e) {
+        console.log("Drop Fierd");
         this._isDragOverFirst = true;
         this._dragItem = null;
     },
@@ -50,4 +68,43 @@ window.bos.gridDrop = {
     getHoveredCell: () => this._hoveredCell,
     getHoveredIndex: () => this._hoveredIndex,
     isDragging: () => this._isDragging
+};
+
+window.Bos = window.Bos || {};
+window.Bos.DragedItem = null;
+window.Bos.Dragable = {
+    init: function(id, dotnetHelper) { 
+        let target = document.getElementById(id);
+        target.addEventListener("drag", () => {
+            dotnetHelper.invokeMethodAsync("OnDrag", "dragData");
+        });
+        target.addEventListener("dragstart", () => {
+            window.Bos.DragedItem = target;
+            dotnetHelper.invokeMethodAsync("OnDragStart", "dragstartData");
+        });
+    }
+};
+window.Bos.DropZone = {
+    init: function(id, dotnetHelper) { 
+        let target = document.getElementById(id);
+        target.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            dotnetHelper.invokeMethodAsync("OnDragOver", "dragoverData");
+        });
+        target.addEventListener("dragenter", () => {
+            target.classList.add("highlighted");
+            dotnetHelper.invokeMethodAsync("OnDragEnter", "dragenterData");
+        }); 
+        target.addEventListener("dragleave", () => {
+            dotnetHelper.invokeMethodAsync("OnDragLeave", "dradleaveData");
+            target.classList.remove("highlighted")
+        }); 
+        target.addEventListener("drop", () => {
+            dotnetHelper.invokeMethodAsync("OnDrop", "dropData");
+            if (window.Bos.DragedItem) target.append(window.Bos.DragedItem);
+        }); 
+        target.addEventListener("dragend", () => {
+            dotnetHelper.invokeMethodAsync("OnDragEnd", "dragendData");
+        });
+    }
 };
